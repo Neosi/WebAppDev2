c81e5372449aab033e2813d80738f7c34ebd8e75
Max Larkin <maxarastir@gmail.com>
2019-10-30 15:19:27 +0000

    First implementation of backend database structure

--- server/entities.py
+++ server/entities.py
@@ -1,39 +1,60 @@
-from pony.orm import Database, PrimaryKey, Set, Required
+from pony.orm import Database, Required, Optional, PrimaryKey, Set
 
 db = Database()
 db.bind(provider='sqlite', filename='database.db', create_db=True)
 
-class Character(db.Entity):
-    id = PrimaryKey(int, auto=True)
-
 class Class(db.Entity):
     id = PrimaryKey(int, auto=True)
+    name = Optional(str)
+    characters = Set('Character')
 
 class Allignment(db.Entity):
     id = PrimaryKey(int, auto=True)
+    name = Optional(str)
+    characters = Set('Character')
 
 class Race(db.Entity):
     id = PrimaryKey(int, auto=True)
-
-class AffiliationList(db.Entity):
-    id = PrimaryKey(int, auto=True)
-
-class Tag(db.Entity):
-    id = PrimaryKey(int, auto=True)
-
-class TagList(db.Entity):
-    id = PrimaryKey(int, auto=True)
+    name = Optional(str)
+    characters = Set('Character')
 
 class Ideal(db.Entity):
     id = PrimaryKey(int, auto=True)
+    description = Required(str)
+    character = Set('Character')
 
-class Desire(db.Entity):
+class Bond(db.Entity):
     id = PrimaryKey(int, auto=True)
+    description = Required(str)
+    character = Set('Character')
 
-class Fear(db.Entity):
+class Flaw(db.Entity):
     id = PrimaryKey(int, auto=True)
+    description = Required(str)
+    character = Set('Character')
 
 class Trait(db.Entity):
     id = PrimaryKey(int, auto=True)
+    description = Required(str)
+    character = Set('Character')
+
+class Character(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    name = Optional(str)
+    age = Optional(int)
+    affiliations = Set("Character", reverse='affiliations')
+    character_class = Optional(Class)
+    race = Optional(Race)
+    allignment = Optional(Allignment)
+    ideals = Set(Ideal)
+    bonds = Set(Bond)
+    flaws = Set(Flaw)
+    traits = Set(Trait)
+    tags = Set('Tag')
+
+class Tag(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    name = Required(str)
+    character = Set(Character)
 
 db.generate_mapping(create_tables=True)
