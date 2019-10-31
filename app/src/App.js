5780795e63d207c57896614cfd9e1693a8b6b5db
Max Larkin <maxarastir@gmail.com>
2019-10-31 21:04:18 +0000

    Seperating react components to appropriate "stories"

--- app/src/App.js
+++ app/src/App.js
@@ -1,80 +1,31 @@
 import React from "react";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import { Layout, Menu, Breadcrumb, Icon } from "antd";
+import { Sidebar } from "./components/sidebar.stories"
+import { Heading } from "./components/header.stories"
 const { SubMenu } = Menu;
-const { Header, Content, Sider } = Layout;
+const { Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <Layout style={{ height: "100vh" }}>
-        <Header className="header" style={{ height: "48px" }}>
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={["1"]}
-            style={{ lineHeight: "48px" }}
-          >
-            <Menu.Item key="1">tbFLIP Character Builder</Menu.Item>
-          </Menu>
-        </Header>
-        <Layout>
-          <Sider width={200} style={{ background: "#fff" }}>
-            <Menu
-              mode="inline"
-              defaultSelectedKeys={["1"]}
-              defaultOpenKeys={["sub1"]}
-              style={{ height: "100%", borderRight: 0 }}
-            >
-              <SubMenu
-                key="sub1"
-                title={
-                  <span>
-                    <Icon type="user" />
-                    Characters
-                  </span>
-                }
-              >
-                <Menu.Item key="1">New</Menu.Item>
-                <Menu.Item key="2">View</Menu.Item>
-              </SubMenu>
-              <SubMenu
-                key="sub2"
-                title={
-                  <span>
-                    <Icon type="idcard" />
-                    Attributes
-                  </span>
-                }
-              >
-                <Menu.Item key="3">Races</Menu.Item>
-                <Menu.Item key="4">Personality</Menu.Item>
-              </SubMenu>
-              <SubMenu
-                key="sub3"
-                title={
-                  <span>
-                    <Icon type="tags" />
-                    Relations
-                  </span>
-                }
-              >
-                <Menu.Item key="5">Tags</Menu.Item>
-                <Menu.Item key="6">Relationships</Menu.Item>
-              </SubMenu>
-            </Menu>
-          </Sider>
-          <Layout style={{ padding: "0 24px 24px" }}>
-            <Breadcrumb style={{ margin: "16px 0" }}>
-              <Breadcrumb.Item>Home</Breadcrumb.Item>
-              <Breadcrumb.Item>List</Breadcrumb.Item>
-              <Breadcrumb.Item>App</Breadcrumb.Item>
-            </Breadcrumb>
-            <Content className="content">Content</Content>
+      <Router>
+        <Layout style={{ height: "100vh" }}>
+          <Heading/>
+          <Layout>
+            <Sidebar/>
+            <Layout style={{ padding: "0 24px 24px" }}>
+              <Breadcrumb style={{ margin: "16px 0" }}>
+                <Breadcrumb.Item>Home</Breadcrumb.Item>
+                <Breadcrumb.Item>List</Breadcrumb.Item>
+                <Breadcrumb.Item>App</Breadcrumb.Item>
+              </Breadcrumb>
+              <Content className="content">Content</Content>
+            </Layout>
           </Layout>
         </Layout>
-      </Layout>
+      </Router>
     </div>
   );
 }
