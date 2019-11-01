f0f1ae9741abda316c46e7057511831b81f4a0e1
Max Larkin <maxarastir@gmail.com>
2019-11-01 15:14:56 +0000

    Added package to handle CORS

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from pony.flask import Pony
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_pyfile('flask.cfg', silent=True)
 Pony(app)
+CORS(app)
 
 import server.views
