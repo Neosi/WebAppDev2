970d5aa54d2eff4c6d157a5c8eaa8897c8a307fa
Max Larkin <maxarastir@gmail.com>
2019-11-03 03:38:48 +0000

    Removed unneccesary binding from api requests

--- app/src/requests.js
+++ app/src/requests.js
@@ -1,25 +1,23 @@
 import axios from "axios";
 
-const url = "http://127.0.0.1:5000";
+export const url = "http://127.0.0.1:5000";
 
 // ---------------------------------------------------
 // CHARACTER
 // ---------------------------------------------------
 export async function removeCharacter(id) {
-  await axios.post(`${url}/remove-character`, { id });
-  this.init();
+  return axios.post(`${url}/remove-character`, { id });
 }
 
-export async function createCharacter(name) {
-  await axios.post(`${url}/add-character`, name);
-  this.init();
+export function createCharacter(data) {
+  return axios.post(`${url}/add-character`, data);
 }
 
 export function getCharacters() {
-  axios
+  return axios
     .get(`${url}/get-characters`)
     .then(response => {
-      this.setState({ characters: response.data, hasData: true });
+      return response.data;
     })
     .catch(error => console.log(error));
 }
@@ -42,6 +40,29 @@ export function getRaces() {
     .get(`${url}/get-races`)
     .then(response => {
       this.setState({ races: response.data, hasData: true });
+    })
+    .catch(error => console.log(error));
+}
+
+
+// ---------------------------------------------------
+// Class
+// ---------------------------------------------------
+export async function removeClass(id) {
+  await axios.post(`${url}/remove-class`, { id });
+  this.init();
+}
+
+export async function createClass(name) {
+  await axios.post(`${url}/add-class`, name);
+  this.init();
+}
+
+export function getClasses() {
+  axios
+    .get(`${url}/get-classes`)
+    .then(response => {
+      this.setState({ classes: response.data, hasData: true });
     })
     .catch(error => console.log(error));
 }
