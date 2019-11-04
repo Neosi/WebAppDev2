135f932023f2a48b374843f7e0596334636aba9d
Max Larkin <maxarastir@gmail.com>
2019-11-04 02:57:11 +0000

    Character page recieves relvant information and formatting

--- app/src/components/character.view.js
+++ app/src/components/character.view.js
@@ -1,18 +1,62 @@
-import React from 'react';
+import React from "react";
+import { getCharacter } from "../requests";
+import { Typography, Row, Col } from "antd";
 
-export default class CharacterView extends React.PureComponent{
-    constructor(props){
-        super(props);
-        this.state={id:window.location.pathname.slice(11)}
-    }
+const { Title, Paragraph } = Typography;
 
-    componentDidMount(){
+export default class CharacterView extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: window.location.pathname.slice(11),
+      character: {},
+      race: {},
+      class: {}
+    };
+  }
 
-    }
+  async componentWillMount() {
+    await getCharacter(this.state.id).then(data => {
+      console.log(data.character_class.name);
+      this.setState({
+        character: data,
+        race: data.race,
+        class: data.character_class
+      });
+    });
+    console.log(this.state.character);
+  }
 
-    render(){
-        return(
-            <div>{this.state.id}</div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Row>
+          <Col span={10}>
+            <Title style={{ textAlign: "left" }}>
+              {this.state.character.name}
+            </Title>
+          </Col>
+          <Col span={14}>
+            <Title level={4} type="secondary">
+              Race: {this.state.race.name} 
+            </Title>
+            <Title level={4} type="secondary">
+              Class: {this.state.class.name} 
+            </Title>
+            <Title level={4} type="secondary">
+              Age: {this.state.character.age}
+            </Title>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Paragraph style={{ textAlign: "left" }} editable >
+              <b>Start Writing! </b>
+              {this.state.character.background}
+            </Paragraph>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
